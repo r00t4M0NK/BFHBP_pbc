c44c7383ccc349fc797f17d2bfaf27d62ea01ba1
r00t4M0NK <4m0nk@proton.me>
2024-09-20 15:12:01 +0200

    Add files via upload

--- tools/bin/cyberhacktoolsinstall.sh
+++ tools/bin/cyberhacktoolsinstall.sh
@@ -14,12 +14,12 @@
 # For Downloading parts: choice to store in a dedicated dir
 ################################################################
 # You need to run these commands in order to download this script in the target machine for adding these tools
-# WSL> export INSTALL_SH_MYTOOLS=https://github.com/r00t4M0NK/BFHBP_pbc/blob/main/tools/bin/cyberhacktoolsinstall.sh
-# WSL> mkdir -p $HOME/internetdl; cd $HOME/internetdl
-#Copy and run in WSL only one line: here part if you have issue under a Firewall:
-# WSL> wget --timeout=5 --tries=2 --no-check-certificate -O cyberhacktoolsinstall.sh -c $INSTALL_SH_MYTOOLS
-#Copy and run in WSL only one line: here usual part without Firewall (more secure to avoid "man in the middle"):
-# WSL> wget --timeout=5 --tries=2 -O cyberhacktoolsinstall.sh -c $INSTALL_SH_MYTOOLS
+# COMET-BASH> export INSTALL_SH_MYTOOLS=https://github.com/r00t4M0NK/BFHBP_pbc/blob/main/tools/bin/cyberhacktoolsinstall.sh
+# COMET-BASH> mkdir -p $HOME/internetdl; cd $HOME/internetdl
+#Copy and run in COMET env only one line: here part if you have issue under a Firewall:
+# COMET-BASH> wget --timeout=5 --tries=2 --no-check-certificate -O cyberhacktoolsinstall.sh -c $INSTALL_SH_MYTOOLS
+#Copy and run in COMET env only one line: here usual part without Firewall (more secure to avoid "man in the middle"):
+# COMET-BASH> wget --timeout=5 --tries=2 -O cyberhacktoolsinstall.sh -c $INSTALL_SH_MYTOOLS
 
 ################################################################
 # Analyse each script from KALI
